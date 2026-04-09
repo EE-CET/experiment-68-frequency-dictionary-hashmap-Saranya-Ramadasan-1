03685852616bb787d4bbff6f39632cec82161aa1
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-09 15:43:41 +0530

    4t commit

--- src/Solution.java
+++ src/Solution.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Integer> frequencyMap = new TreeMap<>();
 
-        while (scanner.hasNext()) {
-            String word = scanner.next();
-            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
-        }
+        if (scanner.hasNextLine()) {
+            String text = scanner.nextLine();
+            String[] words = text.trim().split("\\s+");
 
-        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            Map<String, Integer> frequencyMap = new TreeMap<>();
+
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+                }
+            }
+
+            for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
         }
-        
         scanner.close();
     }
 }
